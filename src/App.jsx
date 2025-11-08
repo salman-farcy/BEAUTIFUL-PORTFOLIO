2e5b401e61cff5ac69071f15ef1388d40b1708fb
slaman-farcy <salmanfarcy253@gmail.com>
2025-11-08 16:04:04 +0600

    feat(pages): add Home and NotFound pages with basic layout and routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+import { BrowserRouter, Route, Routes } from "react-router"
+import Home from "./pages/Home"
+import NotFound from "./pages/NotFound"
 
 
 function App() {
@@ -5,8 +8,12 @@ function App() {
 
   return (
     <>
-    
-        
+        <BrowserRouter>
+          <Routes>
+            <Route index element={<Home />}/>
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
     </>
   )
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+          {/* Theme Toggle */}
+
+
+          {/* Background Effects */}
+
+
+          {/* NavBar */}
+
+
+          {/* Main Content */}
+
+
+          {/* Footer */}
+      
+    </div>
+  )
+}
+
+export default Home
